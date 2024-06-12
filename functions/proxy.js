35b14c8561ef41abaa3b49799484c50971e98c02
IreneKovaleva <kovalevairina357@gmail.com>
2024-06-12 12:02:52 +0300

    change(2) proxy settings for netlify

---
+++ functions/proxy.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const { path } = event;
+  const targetUrl = `https://your-api-endpoint.com${path.replace('/api', '')}`;
+
+  try {
+    const response = await fetch(targetUrl);
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.toString(),
+    };
+  }
+};
